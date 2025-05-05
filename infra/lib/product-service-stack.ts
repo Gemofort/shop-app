968eaf8100a23630b9ccc459ffdb8c92cd84952d
Ivan Feofanov <vanya6677@gmail.com>
2025-05-05 19:37:47 +0300

    Add DynamoDB connection, product creation endpoints, FE update

--- infra/lib/product-service-stack.ts
+++ infra/lib/product-service-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ProductService } from './product-service';
+import { ProductServiceStackProps } from '../interfaces/product-service-stack-props';
 
 export class ProductServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: ProductServiceStackProps) {
     super(scope, id, props);
 
     new ProductService(this, 'ProductService');
